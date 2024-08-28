a6a3a7e5e11bebd6e4d679f6ac28e9c1fdf48918
AlexArciszewski <arciszew@yahoo.com>
2024-08-28 13:40:41 +0200

    SMTP config yahoo

--- edenthought/settings.py
+++ edenthought/settings.py
@@ -149,3 +149,26 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# SMTP Configuration
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mail.yahoo.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+
+EMAIL_HOST_USER = 'arciszew@yahoo.com' #gmail email
+EMAIL_HOST_PASSWORD ='dplialkhoepbmskg'      #APP hasło
+
+DEFAULT_FORM_EMAIL = 'arciszew@yahoo.com'  #gmail email
+
+
+
+
+
+
+
+
+
